0d37320ef788ae663b207bc6fbd3bf9100cb9bf1
Amaan Shaikh <shaikhamaan257@gmail.com>
2018-07-09 17:16:05 +0530

    Navbar Added with routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes}  from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+
+const routes : Routes = [
+  {
+    path : '',
+    component : SearchComponent
+  },
+  {
+    path : 'about',
+    component : AboutComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
